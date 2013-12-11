768d021729adccb37a3e5aab79b71bee85f1ba8e
alexkh <kharlamovalexandre@gmail.com>
2013-12-11 15:26:36 +0600

    Create dummy.cpp

---
+++ dummy.cpp
@@ -0,0 +1,123 @@
+// X=black (goes first), O=white, columns represented by lower-case letter,
+// black connects top and bottom
+#include <iostream>
+#include <string>
+#include <sstream> // reading integer from string
+#include <limits> // streamsize
+#include <locale> // isalpha
+#include <unistd.h> // sleep() - simulates ai's thinking process
+using namespace std;
+
+int autoplay(char color, unsigned short board_side = 11, size_t iter = 1000) {
+	char column; // letter representing board column from a-z
+	unsigned short col; // numeric column
+	unsigned short row; // numeric row
+	char c; // used for input processing
+	// send handshake message color: name of program by author
+	// this string should uniquely identify the player
+	cout << color << ": dummy player by Alexandre Kharlamov\n" << flush;
+	if(color == 'X') {
+		// wait for other player's handshake message
+		cin >> c; // should be the other player's color
+		if(c != 'O') {
+			cout << "X. E: expecting handshake message from O\n"
+				<< flush;
+			return -2;
+		}
+		cin >> c; // should be ':'
+		if(c != ':') {
+			cout << "X. E: expecting : after O in "
+				"handshake message\n" << flush;
+			return -3;
+		}
+		// ignore the rest of the line
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		// make a move
+		cout << color << "a2 #1\n" << flush;
+	}
+	int counter = 1; // count the moves
+	while(true) {
+		cin >> c; // other player color
+		cin >> column; // lower case letter represenging board column
+		if(c != (color=='O'? 'X': 'O') || column == ':') {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+		if(column == '.') { // the other player quits, game over
+			break;
+		}
+		col = column - 'a';
+		if(col >= board_side) {
+			cout << color <<  ". E: " << color <<
+			" received illegal column: '" << c << "'\n";
+			return -4;
+		}
+		cin >> row;
+		if(row > board_side) {
+			cout << color << ". E: " << color <<
+			" received illegal row: '" << row << "'\n";
+			return -5;
+		}
+		c = cin.peek();
+		if(c == '.') { // dot at the end of the other player's move
+			// means that he wins, or maybe he gives up - game over
+			break;
+		}
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		if(color == 'X') {
+			++counter;
+		}
+		// make a move. If I won, add a dot. dummy exits after 3 moves.
+		sleep(1);
+		if(counter < 3) {
+			cout << color << "a3 #" << counter << "\n" << flush;
+		} else {
+			cout << color << "a3.#" << counter << "\n" << flush;
+			break;
+		}
+		if(color == 'O') {
+			++counter;
+		}
+	}
+	return 0;
+}
+
+// usage: <program name> (X|O) [<board side>] [<iterations>]
+// example: hex X 11 1000
+int main(int argc, char *argv[]) {
+	char color = 'X'; // can be X or O
+	unsigned short board_side = 11; // side of the board minimum 3
+	size_t iter = 1000; // number of iterations should be selectable
+	// parse command line parameters
+	argc = argc > 4? 4: argc; // forward compatibility measure
+	switch(argc) {
+	case 4:
+	{
+		stringstream ss; // used for reading numbers from strings
+		ss << argv[3]; // put third argument into the stringstream
+		ss >> iter; // read its numeric value
+	}
+	case 3:
+	{
+		stringstream ss;
+		ss << argv[2];
+		ss >> board_side;
+		board_side = board_side < 3? 3: board_side; // minimum 3
+	}
+	case 2:
+		color = argv[1][0];
+		if(color != 'X' && color != 'O') {
+			cerr << "E: first argument must be X or O\n";
+			return -1; // there is some error
+		}
+		autoplay(color, board_side, iter);
+		return 0;
+	case 1: ; // no command line arguments - continue with interactive play
+	}
+	// optional interactive play code goes here
+	return 0;
+}
+
